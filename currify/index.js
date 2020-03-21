e581630b8c6259c2517f201da5e9c2ea26667096
EvanBell <876895046@qq.com>
2020-03-21 13:02:35 +0800

    添加柯里化函数

---
+++ currify/index.js
@@ -0,0 +1,24 @@
+// 支持单/多参数
+const add = (a, b, c) => a + b + c
+const currifyAdd = currify(add)
+currifyAdd(1,2)(3)
+currifyAdd(1)(2)(3)
+
+// ES6
+const currify = (fn, params = []) =>
+	(...args) => 
+		params.length + args.length === fn.length
+			? fn(...args, ...params)
+      : currify(fn, [...args, ...params])
+      
+
+// ES5
+function currify (fn, paramsArg) {
+  var params = paramsArg || []
+  return function() {
+    var args = arguments
+    return params.length + args.length === fn.length
+        ? fn(...args, ...params)
+        : currify(fn, [...params, ...args])
+  }
+}  
