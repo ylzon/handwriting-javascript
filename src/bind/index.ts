b97b3563cd6dd843b1f7f0bfac022df290789733
EvanBell <876895046@qq.com>
2020-04-13 11:49:07 +0800

    添加bind的ES3+语法

--- src/bind/index.ts
+++ src/bind/index.ts
@@ -1,8 +1,27 @@
+var slice = Array.prototype.slice;
 /**
  * bind
- * @param args
  */
-function bind(...args) {
+function bind(newThis) {
+  var fn = this
+  var args = slice.call(arguments,1)
+
+  if (typeof fn !== 'function') {
+    throw new TypeError('bind Must be called on the function.')
+  }
+
+  function resultFn() {
+    var isNew = resultFn.prototype.isPrototypeOf(this)
+    var args2 = slice.call(arguments, 0)
+    return fn.apply(isNew ? this : newThis, args.concat(args2))
+  }
+
+  resultFn.prototype = fn.prototype
+  return resultFn
+}
+
+// ES6
+function _bind(...args) {
   const fn = this
   const newThis = args[0]
   function resultFn(...args2) {
